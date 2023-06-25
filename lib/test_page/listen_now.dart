f5ebb2b1a8092642442275dc21b340cd25ba89fd
MohanndAnuyounes <mohannad.abuyounes8@gmail.com>
2023-06-25 16:33:12 +0300

    mm

--- lib/test_page/listen_now.dart
+++ lib/test_page/listen_now.dart
@@ -1,24 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:voice_note/helper.dart';
 
 class ListenNow extends StatelessWidget {
   final String frequency;
 
   ListenNow({required this.frequency});
 
-  void _openRadioApp() async {
-    final String radioAppUrlScheme = 'radioapp://frequency=$frequency';
-
-    if (await canLaunch(radioAppUrlScheme)) {
-      final PermissionStatus status = await Permission.microphone.request();
-      if (status.isGranted) {
-        await launch(radioAppUrlScheme);
-      } else {
-        print('Permission denied to access the microphone.');
-      }
+  Future<void> openRadioAppWithFrequency(String frequency) async {
+    final radioAppUrl = 'radioapp://tune?frequency=$frequency';
+    if (await canLaunch(radioAppUrl)) {
+      await launch(radioAppUrl);
     } else {
-      print('Could not launch the radio application.');
+      HelperFunctions.showToastMessage(
+          message: throw 'Could not launch the radio application');
+
     }
   }
 
@@ -33,7 +30,9 @@ class ListenNow extends StatelessWidget {
       body: Center(
         child: MaterialButton(
           color: Colors.blueGrey,
-          onPressed: _openRadioApp,
+          onPressed: () {
+            openRadioAppWithFrequency('98.5%20FM');
+          },
           child: Text(
             'Open Radio App with $frequency',
             style: TextStyle(color: Colors.white),
